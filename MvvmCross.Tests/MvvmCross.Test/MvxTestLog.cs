da7f0c46318dbd5593f9ceb83232b49bbd443a52
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-01-29 21:55:44 +0100

    Add MvvmCross.Test project with shared Test stuff

--- MvvmCross.Tests/MvvmCross.Test/MvxTestLog.cs
+++ MvvmCross.Tests/MvvmCross.Test/MvxTestLog.cs
@@ -10,7 +10,7 @@ using MvvmCross.Platform.Logging;
 
 namespace MvvmCross
 {
-    internal static class MvxTestLog
+    public static class MvxTestLog
     {
         internal static IMvxLog Instance { get; } = Mvx.Resolve<IMvxLogProvider>().GetLogFor("MvxTest");
     }
