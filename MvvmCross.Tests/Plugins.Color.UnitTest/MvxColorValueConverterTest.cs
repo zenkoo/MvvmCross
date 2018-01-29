a69724f2113c553d143c0bc43748b174bd934b0b
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-01-29 22:38:21 +0100

    Run color tests in same collection

--- MvvmCross.Tests/Plugins.Color.UnitTest/MvxColorValueConverterTest.cs
+++ MvvmCross.Tests/Plugins.Color.UnitTest/MvxColorValueConverterTest.cs
@@ -6,12 +6,21 @@
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
 using MvvmCross.Platform.UI;
-using MvvmCross.Test.Core;
+using MvvmCross.Test;
+using Xunit;
 
 namespace MvvmCross.Plugins.Color.Test
 {
-    public class MvxColorValueConverterTest : MvxIoCSupportingTest
+    public class MvxColorValueConverterTest : IClassFixture<MvxTestFixture>
     {
+        public MvxTestFixture Fixture { get; }
+
+        public MvxColorValueConverterTest(MvxTestFixture fixture)
+        {
+            Fixture = fixture;
+            Fixture.Ioc.RegisterSingleton<IMvxNativeColor>(new MockNative());
+        }
+
         public class WrappedColor
         {
             public WrappedColor(MvxColor color)
@@ -29,11 +38,13 @@ namespace MvvmCross.Plugins.Color.Test
                 return new WrappedColor(mvxColor);
             }
         }
+    }
 
-        protected override void AdditionalSetup()
-        {
-            base.AdditionalSetup();
-            Ioc.RegisterSingleton<IMvxNativeColor>(new MockNative());
-        }
+    [CollectionDefinition("Color")]
+    public class DatabaseCollection : ICollectionFixture<MvxTestFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
     }
 }
